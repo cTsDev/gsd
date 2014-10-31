536c7a826d81f9fbaba30f3f9388427400cc0da5
DaneEveritt <dane@daneeveritt.com>
2014-10-31 19:00:51 -0400

    Maybe enable FTPS?

--- interfaces/ftp.js
+++ interfaces/ftp.js
@@ -3,18 +3,48 @@ var config = require('../config.json');
 var ftpconfig = config.interfaces.ftp;
 var request = require('request');
 var hasPermission = require('../auth.js').hasPermission;
+var dotenv = require('dotenv');
+dotenv.load();
 
 String.prototype.rsplit = function(sep, maxsplit) {
     var split = this.split(sep);
     return maxsplit ? [ split.slice(0, -maxsplit).join(sep) ].concat(split.slice(-maxsplit)) : split;
 };
 
+if (process.env.KEY_FILE && process.env.CERT_FILE) {
+  console.log('Running as FTPS server');
+  if (process.env.KEY_FILE.charAt(0) !== '/') {
+    keyFile = path.join(__dirname, process.env.KEY_FILE);
+  }
+  if (process.env.CERT_FILE.charAt(0) !== '/') {
+    certFile = path.join(__dirname, process.env.CERT_FILE);
+  }
+  options.tls = {
+    key: fs.readFileSync(keyFile),
+    cert: fs.readFileSync(certFile),
+    ca: !process.env.CA_FILES ? null : process.env.CA_FILES
+      .split(':')
+      .map(function (f) {
+        return fs.readFileSync(f);
+      })
+  };
+}
+else {
+  console.log();
+  console.log('*** To run as FTPS server,                 ***');
+  console.log('***  set "KEY_FILE", "CERT_FILE"           ***');
+  console.log('***  and (optionally) "CA_FILES" env vars. ***');
+  console.log();
+}
+
 var options = {
-  //pasvPortRangeStart: 4000,
-  //pasvPortRangeEnd: 5000,
+  pasvPortRangeStart: 4000,
+  pasvPortRangeEnd: 5000,
   getInitialCwd: function(user) {
     return "/"
   },
+  tlsOptions: options.tls,
+  allowUnauthorizedTls: true,
   getRoot: function(connection) {
     split = connection.username.rsplit("-",1);
     username = split[0];
@@ -24,6 +54,10 @@ var options = {
 };
 
 var server = new ftpd.FtpServer(ftpconfig.host, options);
+
+server.on('error', function (error) {
+  console.log('FTP Server error:', error);
+});
 
 server.on('client:connected', function(conn) {
   var username;
