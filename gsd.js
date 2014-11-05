78db8eb4dd8acff12304ecb6c0ff9077c24ff47e
Dane Everitt <DaneEveritt@users.noreply.github.com>
2014-11-05 13:48:17 -0500

    Update FTP interface
    Require FTPS connections

--- gsd.js
+++ gsd.js
@@ -2,6 +2,6 @@ var config = require('./config.json');
 
 require('./interfaces/console.js');
 require('./interfaces/rest.js');
-// require('./interfaces/ftp.js');
+require('./interfaces/ftp.js');
 
 var servers = require('./services');
--- interfaces/ftp.js
+++ interfaces/ftp.js
@@ -1,128 +1,108 @@
-fs = require('fs');
-path = require('path');
+var fs = require('fs');
+var path = require('path');
 var ftpd = require('ftpd');
-var optns = {tls: null};
 var config = require('../config.json');
 var ftpconfig = config.interfaces.ftp;
 var request = require('request');
 var hasPermission = require('../auth.js').hasPermission;
-var dotenv = require('dotenv');
-dotenv.load();
 
 String.prototype.rsplit = function(sep, maxsplit) {
     var split = this.split(sep);
     return maxsplit ? [ split.slice(0, -maxsplit).join(sep) ].concat(split.slice(-maxsplit)) : split;
 };
 
-if (process.env.KEY_FILE && process.env.CERT_FILE) {
-  console.log('Running as FTPS server');
-  if (process.env.KEY_FILE.charAt(0) !== '/') {
-    keyFile = path.join(__dirname, process.env.KEY_FILE);
-  }else{
-    keyFile = process.env.KEY_FILE;
-  }
-  if (process.env.CERT_FILE.charAt(0) !== '/') {
-    certFile = path.join(__dirname, process.env.CERT_FILE);
-  }else{
-    certFile = process.env.CERT_FILE;
-  }
-  optns.tls = {
-    key: fs.readFileSync(keyFile),
-    cert: fs.readFileSync(certFile),
-    ca: !process.env.CA_FILES ? null : process.env.CA_FILES
-      .split(':')
-      .map(function (f) {
-        return fs.readFileSync(f);
-      })
-  };
-console.log('*** RUNNNING FTPS ***');
-}
-else {
-  console.log();
-  console.log('*** To run as FTPS server,                 ***');
-  console.log('***  set "KEY_FILE", "CERT_FILE"           ***');
-  console.log('***  and (optionally) "CA_FILES" env vars. ***');
-  console.log();
+if(!fs.existsSync('../ftps.key') || !fs.existsSync('../ftps.pem')){
+    console.log(' ** WARNING: NO KEY OR CERT FILE DETECTED! **');
 }
 
 var options = {
-  //pasvPortRangeStart: 4000,
-  //pasvPortRangeEnd: 5000,
-  getInitialCwd: function(user) {
-    return "/"
-  },
-  tlsOptions: optns.tls,
-  allowUnauthorizedTls: true,
-  getRoot: function(connection) {
-    split = connection.username.rsplit("-",1);
-    username = split[0];
-    serverId =  split[1];
-    return config.servers[serverId].path;
-  }
+    pasvPortRangeStart: 4000,
+    pasvPortRangeEnd: 5000,
+    getInitialCwd: function(user) {
+        return "/"
+    },
+    tlsOptions: {
+        key: fs.readFileSync('../ftps.key'),
+        cert: fs.readFileSync('../ftps.pem')
+    },
+    allowUnauthorizedTls: true,
+    getRoot: function(connection) {
+        split = connection.username.rsplit("-",1);
+        username = split[0];
+        serverId =  split[1];
+        return config.servers[serverId].path;
+    }
 };
 
-var server = new ftpd.FtpServer(ftpconfig.host, options);
+try {
+    var server = new ftpd.FtpServer(ftpconfig.host, options);
+} catch(ex) {
+    console.log("Exception occured trying to start FTP server.");
+    cosole.log(ex);
+    failure();
+}
 
 server.on('error', function (error) {
-  console.log('FTP Server error:', error);
+    console.log('FTP Server error:', error);
 });
 
 server.on('client:connected', function(conn) {
-  var username;
-  var serverId;
-  var fullUsername;
-  console.log('Client connected from ' + conn.socket.remoteAddress);
+    var username;
+    var serverId;
+    var fullUsername;
 
-  conn.on('command:user', function(user, success, failure) {
-    if (user.indexOf("-") == -1){
-        failure()
-    }
+    console.log('Client connected from ' + conn.socket.remoteAddress);
 
-    split = user.rsplit("-",1);
-    username = split[0];
-    serverId =  split[1];
+    conn.on('command:user', function(user, success, failure) {
+        if (user.indexOf("-") == -1){
+            failure()
+        }
 
-    try {
-        serverId = parseInt(serverId);
-    }catch(ex){
-        failure();
-    }
+        fullUsername = user;
+        split = user.rsplit("-",1);
+        username = split[0];
+        serverId =  split[1];
 
-    fullUsername = user;
+        try {
+            serverId = parseInt(serverId);
+        }catch(ex){
+            failure();
+        }
 
-    success()
-  });
+        success();
+    });
 
-  conn.on('command:pass', function(pass, success, failure) {
-      if (ftpconfig.authurl != null){
-          request.post(ftpconfig.authurl, {form:{username:fullUsername, password:pass}}, function (error, response, body) {
-              if (!error && response.statusCode == 200) {
-                  try {
-                      res = JSON.parse(body);
-                      if (res.authkey != null){
-                          if (hasPermission("ftp", res.authkey, serverId)){
-                              success(username + "-" + serverId);
-                          }else{
-                              failure();
-                          }
-                      }else{
-                          failure();
-                      }
-                  } catch (ex) {
-                      failure();
-                  }
-              }else{
-                  failure();
-              }
-          });
-      }else{
-          if (hasPermission("ftp", username, serverId)){
-              success(username + "-" + serverId);
-          }else{
-              failure();
-          }
-      }
-  });
+    conn.on('command:pass', function(pass, success, failure) {
+        if (ftpconfig.authurl != null){
+            request.post(ftpconfig.authurl, {form:{username:fullUsername, password:pass}}, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    try {
+                        res = JSON.parse(body);
+                        if (res.authkey != null){
+                            if (hasPermission("ftp", res.authkey, serverId)){
+                                success(username + "-" + serverId);
+                            }else{
+                                console.log("Failed to authenticate: authenticate key does not have permission for this action.");
+                                failure();
+                            }
+                        }else{
+                            console.log("Failed to authenticate: missing authentication key.");
+                            failure();
+                        }
+                    } catch (ex) {
+                        console.log("Failed to authenticate: "+ ex);
+                        failure();
+                    }
+                }else{
+                    console.log("Failed to authenticate: Server returned error code ("+ response.statusCode +").");
+                    failure();
+                }
+            });
+        }else{
+            console.log("Failed to authenticate: no authentication URL provided.");
+            failure();
+        }
+    });
 
 });
 
